765a1997a00f285abbe2f08c0db7a988fb635917
Raj Yadav <rajyadav968940@gmail.com>
2025-07-02 11:01:26 +0530

    Add files via upload

---
+++ Concept/Abstract/Abstract1.java
@@ -0,0 +1,41 @@
+class Abstract1
+{
+    public static void main(String arg[])
+    {
+        Hello hobj = new Hello();
+        hobj.fun();
+        hobj.gun();
+         
+    }
+}
+abstract class Demo
+{
+    public int A;
+    public int B;
+
+    public Demo()
+    {
+        System.out.println("Inside Constructor ");
+        this.A=0;
+        this.B =0;
+
+    }
+    abstract void fun();
+
+    void gun()
+    {
+        System.out.println("Inside Gun of Demo");
+    }
+}
+class Hello extends Demo
+{
+    public Hello()
+    {
+        System.out.println("Inside Hello Constructer");
+    }
+
+    public void fun()
+    {
+        System.out.println("Inside fun of Hello ");
+    }
+}
