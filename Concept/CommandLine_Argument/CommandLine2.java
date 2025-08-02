b3a6f623e6e954ccf45bd31a6cd0c274546e5b39
Raj Yadav <rajyadav968940@gmail.com>
2025-08-02 23:16:30 +0530

    Add files via upload

---
+++ Concept/CommandLine_Argument/CommandLine2.java
@@ -0,0 +1,12 @@
+class CommandLine2
+{
+    public static void main(String arg[])
+    {
+        int iNo1= Integer.parseInt(arg[0]);
+        int iNo2 = Integer.parseInt(arg[1]);
+
+        int iAns=iNo1+ iNo2;
+
+        System.out.println("Addtion is "+iAns);
+    }
+}
